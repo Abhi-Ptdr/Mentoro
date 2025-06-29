faefcd4fbcd75afda4ef7b04358c07edef862976
Abhishek Patidar <abhipatidar253@gmail.com>
2025-06-29 11:51:43 +0530

    Dark theme and header footer component setup

--- Mentoro/app/page.js
+++ Mentoro/app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Abhi</div>
+    <div>Abhi
+
+      <Button>Hello</Button>
+    </div>
   );
 }
